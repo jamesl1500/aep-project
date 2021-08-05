9959edad7caa02e80f84d38a3a496c7b10f1bd2a
James Latten <billing@sitelyftstudios.com>
2021-08-05 17:39:18 -0400

    Another fix

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -3,30 +3,5 @@ $props = DB::table('site_props')->where('id', 1)->get();
 ?>
 <!-- Footer Area -->
 <div class="footerMain">
-    <div class="container innerFooter">
-        <div class="topFooterIcon">
-            <div class="middleIcon">
-                <img src="<?php echo url('images'); ?>/main_logo.jpg" />
-            </div>
-        </div>
-        <div class="middleFooterLinks">
-            <div class="innerFooterLinks">
-
-                <ul>
-                    <li><a href="{{ route('home.index') }}">Home</a></li>
-                    <li><a href="{{ route('about.index') }}">About U.S</a></li>
-                    <li><a href="{{ route('help.index') }}">Help</a></li>
-                    <li><a href="/products">All Products</a></li>
-
-                </ul>
-            </div>
-        </div>
-        <div class="bottomFooterCopyright">
-            <div class="innerFooterCopyright">
-                <p style="color: white;font-weight: bold;"></p>
-                <p style="color: white;"></p>
-                <h3></h3>
-            </div>
-        </div>
-    </div>
+    
 </div>
